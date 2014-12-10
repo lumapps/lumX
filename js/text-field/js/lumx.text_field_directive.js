a856134c0e8aa553b8b29755131e7fc4d2140df5
Thierry Tep <thierry@lumapps.com>
2014-12-10 23:26:44 +0100

    feat text-fields: add textarea in text fields components
    The text area can be used with a type of "area". The text area's
    height grow with the content.

--- js/text-field/js/lumx.text_field_directive.js
+++ js/text-field/js/lumx.text_field_directive.js
@@ -21,10 +21,91 @@ angular.module('lumx.text-field', [])
             replace: true,
             link: function(scope, element, attrs)
             {
+                scope.data = {
+                    focused: false,
+                    model: ''
+                };
+
                 if (angular.isUndefined(attrs.type))
                 {
                     scope.type = 'text';
                 }
+
+                scope.$watch('model', function(newValue)
+                {
+                    if (newValue)
+                    {
+                        scope.data.model = newValue;
+                    }
+                });
+
+                scope.$watch('data.model', function(newValue)
+                {
+                    if (newValue)
+                    {
+                        scope.model = newValue;
+                    }
+                });
+
+                var $textarea, $copy;
+                scope.$watch('type', function(newValue)
+                {
+                    if (newValue === 'area')
+                    {
+                        $textarea = element.find('textarea');
+                        $copy = '<textarea class="textarea-copy"></textarea>';
+
+                        // Scrollbar standard with is 17px : http://www.textfixer.com/tutorials/browser-scrollbar-width.php
+                        var $scrollbarWidth = 17;
+                        
+                        $textarea.width = $textarea.width();
+                        element.append($copy);
+
+                        $copy = element.find('.textarea-copy');
+                        $copy.css({
+                            'height': 34,
+                            'width': $textarea.width + $scrollbarWidth
+                        });
+
+                        $textarea.on('focus', function()
+                        {
+                            var lastHeight = $textarea.height();
+                            $textarea.height(lastHeight + 24);
+                        });
+
+                        $textarea.on('keyup cut paste', function()
+                        {
+                            $textarea.content = $textarea.val();
+                            $copy.val($textarea.content);
+                            var $newHeight = $copy.prop('scrollHeight') + 24;
+                            $textarea.velocity({ 
+                                height: $newHeight
+                            }, {
+                                duration: 50,
+                                easing: 'linear'
+                            });
+                        });
+
+                        $textarea.on('blur', function()
+                        {
+                            var lastHeight = $textarea.height();
+
+                            if (lastHeight == 56)
+                            {
+                                $textarea.height(lastHeight - 24);
+                            }
+                            else
+                            {
+                                $textarea.height(lastHeight - 20);
+                            }
+                        });
+                    }
+                    else if ($textarea)
+                    {
+                        $textarea.off('focus').off('blur').off('keyup cut paste');
+                        $copy.remove();
+                    }
+                });
             }
         };
     }]);
