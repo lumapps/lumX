5fc10a30ae577d3c81d8500606624ebcec9737cc
Matthias Manoukian <matthias@lumapps.com>
2015-09-18 18:03:34 +0200

    fix progress: refactor progress service / directive
    BROKEN:
    When calling progress service / directive, color parameter must be a
    material design color 500 identifier (red, pink, etc.)
    Directive attributes must now be prefixed: type -> lx-type, color ->
    lx-color

---
+++ modules/progress/js/progress_directive.js
@@ -0,0 +1,74 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('lumx.progress')
+        .directive('lxProgress', lxProgress);
+
+    function lxProgress()
+    {
+        var directive =
+        {
+            restrict: 'E',
+            templateUrl: 'progress.html',
+            scope: {
+                lxType: '@?',
+                lxDiameter: '@?',
+                lxColor: '@?',
+            },
+            controller: LxProgressController,
+            controllerAs: 'vm',
+            bindToController: true
+        };
+
+        return directive;
+    }
+
+    function LxProgressController()
+    {
+        var vm = this;
+
+        //
+        // PUBLIC ATTRIBUTES
+        //
+
+        // Public members
+        vm.getProgressDiameter = getProgressDiameter;
+
+        //
+        // PRIVATE METHODS
+        //
+
+        /**
+         * Initialize the controller
+         */
+        function _init()
+        {
+            vm.lxDiameter =  angular.isDefined(vm.lxDiameter) ? vm.lxDiameter : 50;
+            vm.lxColor =  angular.isDefined(vm.lxColor) ? vm.lxColor : 'primary';
+        }
+
+        //
+        // PUBLIC METHODS
+        //
+
+        /**
+         * Get circular progress diameter
+         */
+        function getProgressDiameter()
+        {
+            if (vm.lxType === 'circular')
+            {
+                return { 'transform': 'scale(' + parseInt(vm.lxDiameter) / 100 + ')' };
+            }
+
+            return;
+        }
+
+        //
+        // INITIALIZATION
+        //
+
+        _init();
+    }
+})();
--- modules/progress/js/progress_service.js
+++ modules/progress/js/progress_service.js
@@ -1,174 +1,94 @@
-/* global angular */
-/* global document */
-'use strict'; // jshint ignore:line
+(function() {
+    'use strict';
 
+    angular
+        .module('lumx.progress', [])
+        .service('LxProgressService', LxProgressService);
 
-angular.module('lumx.progress', [])
-    .service('LxProgressService', ['$timeout', '$interval', function($timeout, $interval)
+    LxProgressService.$inject = ['$compile', '$rootScope', '$timeout'];
+
+    function LxProgressService($compile, $rootScope, $timeout)
     {
-        var progressCircularIsShown = false,
-            progressCircular,
-            progressCircularSvg,
-            progressCircularPath,
-            progressLinearIsShown = false,
-            progressLinear,
-            progressLinearBackground,
-            progressLinearFirstBar,
-            progressLinearSecondBar;
-
-        function init()
-        {
-            // Circular
-            progressCircular = document.createElement('div');
-            progressCircular.setAttribute('class', 'progress-circular');
-
-            progressCircularSvg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-            progressCircularSvg.setAttribute('class', 'progress-circular__svg');
-
-            progressCircularPath = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-            progressCircularPath.setAttribute('class', 'progress-circular__path');
-            progressCircularPath.setAttribute('cx', '50');
-            progressCircularPath.setAttribute('cy', '50');
-            progressCircularPath.setAttribute('r', '20');
-            progressCircularPath.setAttribute('fill', 'none');
-            progressCircularPath.setAttribute('stroke-miterlimit', '10');
-
-            progressCircularSvg.appendChild(progressCircularPath);
-            progressCircular.appendChild(progressCircularSvg);
-
-            // Linear
-            progressLinear = angular.element('<div/>', { 'class': 'progress-linear' });
-            progressLinearBackground = angular.element('<div/>', { 'class': 'progress-linear__background' });
-            progressLinearFirstBar = angular.element('<div/>', { 'class': 'progress-linear__bar progress-linear__bar--first' });
-            progressLinearSecondBar = angular.element('<div/>', { 'class': 'progress-linear__bar progress-linear__bar--second' });
-
-            progressLinear
-                .append(progressLinearBackground)
-                .append(progressLinearFirstBar)
-                .append(progressLinearSecondBar);
-        }
-
-        function showCircular(color, container)
-        {
-            if (!progressCircularIsShown)
-            {
-                showCircularProgress(color, container);
-            }
-        }
-
-        function hideCircular()
-        {
-            if (progressCircularIsShown)
-            {
-                hideCircularProgress();
-            }
-        }
-
-        function showCircularProgress(color, container)
-        {
-            progressCircularIsShown = true;
-
-            progressCircularPath.setAttribute('stroke', color);
-
-            if (angular.isDefined(container))
-            {
-                document.querySelector(container).appendChild(progressCircular);
-            }
-            else
-            {
-                document.getElementsByTagName('body')[0].appendChild(progressCircular);
-            }
-
-            $timeout(function()
-            {
-                progressCircular.setAttribute('class', 'progress-circular progress-circular--is-shown');
-            });
-        }
-
-        function hideCircularProgress()
-        {
-            progressCircular.setAttribute('class', 'progress-circular');
-
-            $timeout(function()
-            {
-                progressCircular.remove();
-
-                progressCircularIsShown = false;
-            }, 400);
-        }
-
-        function showLinear(color, container)
-        {
-            if (!progressLinearIsShown)
-            {
-                showLinearProgress(color, container);
-            }
-        }
-
-        function hideLinear()
-        {
-            if (progressLinearIsShown)
-            {
-                hideLinearProgress();
-            }
-        }
-
-        function showLinearProgress(color, container)
-        {
-            progressLinearIsShown = true;
-
-            progressLinearBackground.css({ backgroundColor: color });
-            progressLinearFirstBar.css({ backgroundColor: color });
-            progressLinearSecondBar.css({ backgroundColor: color });
-
-            if (angular.isDefined(container))
-            {
-                progressLinear.appendTo(container);
-            }
-            else
-            {
-                progressLinear.appendTo('body');
-            }
-
-            $timeout(function()
-            {
-                progressLinear.addClass('progress-linear--is-shown');
-            });
-        }
-
-        function hideLinearProgress()
-        {
-            progressLinear.removeClass('progress-linear--is-shown');
-
-            $timeout(function()
-            {
-                progressLinear.remove();
-
-                progressLinearIsShown = false;
-            }, 400);
-        }
-
-        init();
-
-        return {
+        var service = {
             circular: {
-                show: showCircular,
-                hide: hideCircular
+                show: showProgressCircular,
+                hide: hideProgressCircular
             },
             linear: {
-                show: showLinear,
-                hide: hideLinear
+                show: showProgressLinear,
+                hide: hideProgressLinear
             }
         };
-    }])
-    .directive('lxProgress', function()
-    {
-        return {
-            restrict: 'E',
-            scope: {
-                type: '@',
-                color: '@'
-            },
-            templateUrl: 'progress.html'
-        };
-    });
+
+        var _progressCircular;
+        var _progressCircularIsShown = false;
+        var _progressLinear;
+        var _progressLinearIsShown = false;
+
+        return service;
+
+        //
+        // PUBLIC METHODS
+        //
+
+        /**
+         * Hide circular progress
+         */
+        function hideProgressCircular()
+        {
+            _progressCircularIsShown = false;
+            _progressCircular.remove();
+        }
+
+        /**
+         * Hide linear progress
+         */
+        function hideProgressLinear()
+        {
+            _progressLinearIsShown = false;
+            _progressLinear.remove();
+        }
+
+        /**
+         * Show circular progress
+         */
+        function showProgressCircular(color, container)
+        {
+            if (!_progressCircularIsShown)
+            {
+                _progressCircularIsShown = true;
+
+                var progressCircularColor = angular.isDefined(color) ? color : 'primary';
+                var progressCircularContainer = angular.isDefined(container) ? container : 'body';
+
+                _progressCircular = $compile('<lx-progress lx-type="circular" lx-color="' + progressCircularColor + '"></lx-progress>')($rootScope);
+
+                $timeout(function()
+                {
+                    angular.element(progressCircularContainer).append(_progressCircular[0]);
+                });
+            }
+        }
+
+        /**
+         * Show linear progress
+         */
+        function showProgressLinear(color, container)
+        {
+            if (!_progressLinearIsShown)
+            {
+                _progressLinearIsShown = true;
+
+                var progressLinearColor = angular.isDefined(color) ? color : 'primary';
+                var progressLinearContainer = angular.isDefined(container) ? container : 'body';
+
+                _progressLinear = $compile('<lx-progress lx-type="linear" lx-color="' + progressLinearColor + '"></lx-progress>')($rootScope);
+
+                $timeout(function()
+                {
+                    angular.element(progressLinearContainer).append(_progressLinear[0]);
+                });
+            }
+        }
+    }
+})();
