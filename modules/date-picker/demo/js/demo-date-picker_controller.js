a25ebbf14bfe68652ee68bc410e6546bbc7b2dc2
matmkian <matthias.manoukian@gmail.com>
2016-02-03 20:58:13 +0100

    chore demo: update date picker documentation

--- modules/date-picker/demo/js/demo-date-picker_controller.js
+++ modules/date-picker/demo/js/demo-date-picker_controller.js
@@ -6,16 +6,35 @@
         .module('Controllers')
         .controller('DemoDatePickerController', DemoDatePickerController);
 
-    function DemoDatePickerController()
+    DemoDatePickerController.$inject = ['LxDatePickerService'];
+
+    function DemoDatePickerController(LxDatePickerService)
     {
         var vm = this;
 
+        vm.datePickerCallback = datePickerCallback;
+        vm.openDatePicker = openDatePicker;
+
+        vm.locale = 'en';
         vm.datePicker = {
-            date1: new Date(),
-            date2: new Date(),
-            emptyDate: undefined,
+            date: new Date(),
+            dateFormatted: moment().locale(vm.locale).format('LL'),
             minDate: new Date(new Date().getFullYear(), new Date().getMonth() - 2, new Date().getDate()),
             maxDate: new Date(new Date().getFullYear(), new Date().getMonth() + 2, new Date().getDate())
         };
+        vm.datePickerId = 'date-picker';
+
+        ////////////
+
+        function datePickerCallback(_newdate)
+        {
+            vm.datePicker.date = _newdate;
+            vm.datePicker.dateFormatted = moment(_newdate).locale(vm.locale).format('LL');
+        }
+
+        function openDatePicker(_pickerId)
+        {
+            LxDatePickerService.open(_pickerId);
+        }
     }
 })();
