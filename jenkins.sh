08e6614bc2838e9ef8e004efde1e33eadce7d8df
Clément PREVOT <clementp@lumapps.com>
2016-10-11 15:00:59 +0200

    ci(jenkins): add a reason to the build end

--- jenkins.sh
+++ jenkins.sh
@@ -32,6 +32,8 @@ function exitWithCode() {
     printf "Boilerplate CI on branch "${GIT_BRANCH}" done ($(date))\n"
     printf "Exit with code ${1}\n"
 
+    echo "REASON=${REASON}" >> build.properties
+
     exit $1
 }
 
@@ -62,20 +64,25 @@ echo "GIT_EMAIL=${GIT_EMAIL}" >> build.properties
 
 printf "Starting Boilerplate CI on branch '${GIT_BRANCH}' ($(date)) because of '${GIT_NAME} <${GIT_EMAIL}>' changes\n\n"
 
+REASON="The ${setupLabel,,} step failed. Please check the attached build log to see what wents wrong."
 if [[ "$SETUP" != "None" ]]; then
     step "${setupLabel}" "setup${setupType}"
 fi
 
+REASON="A configuration file is not correctly formatted and has been rejected by the linter.<br />Please check the attached build log to see what wents wrong."
 if [[ $LINT == *"config"* ]]; then
     step "Lint configuration" "lint:config"
 fi
+REASON="A source file is not correctly formatted and has been rejected by the linter.<br />Please check the attached build log to see what wents wrong."
 if [[ $LINT == *"source"* ]]; then
     step "Lint sources" "lint:src"
 fi
 
+REASON="At least one unit test has failed and the build has been cancelled.<br />Please check the attached build log to see what wents wrong."
 if [[ $TESTS == *"unit"* ]]; then
     step "Units tests" "unit"
 fi
+REASON="At least one E2E test has failed and the build has been cancelled.<br />Please check the attached build log to see what wents wrong."
 if [[ $TESTS == *"e2e"* ]]; then
     step "E2E tests" "e2e:headless"
 fi
@@ -84,5 +91,6 @@ fi
 tar -czf tests/client/unitReport.tar.gz -C tests/client/unit/report .
 tar -czf tests/client/e2eReport.tar.gz -C tests/client/e2e/report .
 
+REASON="This means that the setup process is fine, lint is OK and every tests have passed. Congrat's!<br />You'll find the tests reports and build log attached to this email."
 
 exitWithCode 0
