45334e450b898b2f3409ddd4fcc280125df5ad33
Clément PREVOT <clementp@lumapps.com>
2016-10-11 15:22:19 +0200

    ci(jenkins): add a failure simulation mechanism
    Also rewrite the terminal color management

--- jenkins.sh
+++ jenkins.sh
@@ -1,11 +1,40 @@
 #!/bin/bash
 
-GRAY="\e[37m"
-BLUE="\e[34m"
-RED="\e[31m"
-GREEN="\e[32m"
-DEFAULT="\e[39m"
+BOLD=""
+UNDERLINE=""
+STANDOUT=""
+DEFAULT=""
+BLACK=""
+RED=""
+GREEN=""
+YELLOW=""
+BLUE=""
+MAGENTA=""
+CYAN=""
+WHITE=""
 
+if [[ "$*" != *"--no-colors"* ]]; then
+    # Check if stdout is a terminal...
+    if test -t 1; then
+        # See if it supports colors...
+        ncolors=$(tput colors)
+
+        if test -n "$ncolors" && test $ncolors -ge 8; then
+            BOLD="$(tput bold)"
+            UNDERLINE="$(tput smul)"
+            STANDOUT="$(tput smso)"
+            DEFAULT="$(tput sgr0)"
+            BLACK="$(tput setaf 0)"
+            RED="$(tput setaf 1)"
+            GREEN="$(tput setaf 2)"
+            YELLOW="$(tput setaf 3)"
+            BLUE="$(tput setaf 4)"
+            MAGENTA="$(tput setaf 5)"
+            CYAN="$(tput setaf 6)"
+            WHITE="$(tput setaf 7)"
+        fi
+    fi
+fi
 
 npmFlags="-s"
 if [[ $VERBOSE == true ]]; then
@@ -18,14 +47,6 @@ setupType=""
 if [[ $SETUP == "Fast" ]]; then
     setupLabel="Fast setup"
     setupType=":fast"
-fi
-
-if [[ "$1" = "--no-colors" ]]; then
-    GRAY=""
-    BLUE=""
-    RED=""
-    GREEN=""
-    DEFAULT=""
 fi
 
 function exitWithCode() {
@@ -65,31 +86,62 @@ echo "GIT_EMAIL=${GIT_EMAIL}" >> build.properties
 printf "Starting Boilerplate CI on branch '${GIT_BRANCH}' ($(date)) because of '${GIT_NAME} <${GIT_EMAIL}>' changes\n\n"
 
 REASON="The ${setupLabel,,} step failed. Please check the attached build log to see what wents wrong."
+if [[ $FAIL == *"setup"* ]]; then
+    printf "${YELLOW}Simulating ${setupLabel,,} failure${DEFAULT}"
+    exitWithCode 1
+fi
 if [[ "$SETUP" != "None" ]]; then
     step "${setupLabel}" "setup${setupType}"
 fi
 
 REASON="A configuration file is not correctly formatted and has been rejected by the linter.<br />Please check the attached build log to see what wents wrong."
+if [[ $FAIL == *"lint:config"* ]]; then
+    printf "${YELLOW}Simulating lint configuration failure${DEFAULT}"
+    exitWithCode 1
+fi
 if [[ $LINT == *"config"* ]]; then
     step "Lint configuration" "lint:config"
 fi
 REASON="A source file is not correctly formatted and has been rejected by the linter.<br />Please check the attached build log to see what wents wrong."
+if [[ $FAIL == *"lint:src"* ]]; then
+    printf "${YELLOW}Simulating lint source failure${DEFAULT}"
+    exitWithCode 1
+fi
 if [[ $LINT == *"source"* ]]; then
     step "Lint sources" "lint:src"
 fi
 
 REASON="At least one unit test has failed and the build has been cancelled.<br />Please check the attached build log to see what wents wrong."
+if [[ $FAIL == *"tests:units"* ]]; then
+    printf "${YELLOW}Simulating units tests failure${DEFAULT}"
+    exitWithCode 1
+fi
 if [[ $TESTS == *"unit"* ]]; then
     step "Units tests" "unit"
 fi
 REASON="At least one E2E test has failed and the build has been cancelled.<br />Please check the attached build log to see what wents wrong."
+if [[ $FAIL == *"tests:e2e"* ]]; then
+    printf "${YELLOW}Simulating E2E tests failure${DEFAULT}"
+    exitWithCode 1
+fi
 if [[ $TESTS == *"e2e"* ]]; then
     step "E2E tests" "e2e:headless"
 fi
 
+REASON="The setup process is fine, lint is OK and every tests have passed. However, tests reports couldn't have been generated.<br />This means that your build is fine, but you should consider checking on the attached build log to see what wents wrong with reports generation."
 
+if [[ $FAIL == *"tests:reports"* ]]; then
+    printf "${YELLOW}Simulating reports generation failure${DEFAULT}"
+    exitWithCode 1
+fi
 tar -czf tests/client/unitReport.tar.gz -C tests/client/unit/report .
+if [[ $? -ne 0 ]]; then
+    exitWithCode $?
+fi
 tar -czf tests/client/e2eReport.tar.gz -C tests/client/e2e/report .
+if [[ $? -ne 0 ]]; then
+    exitWithCode $?
+fi
 
 REASON="This means that the setup process is fine, lint is OK and every tests have passed. Congrat's!<br />You'll find the tests reports and build log attached to this email."
 
