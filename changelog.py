f772d52c94f0817390339ec4eb19d0ea5e292b25
Clément PREVOT <clement@lumapps.com>
2016-10-17 16:46:15 +0200

    build(release): add scripts to generate changelog and release

---
+++ changelog.py
@@ -0,0 +1,106 @@
+#! /usr/bin/env python
+from subprocess import Popen, PIPE
+import re
+
+
+def getTags():
+    Popen('git fetch --tags'.split(), stdout=PIPE).communicate()
+    (stdout, _) = Popen('git tag'.split(), stdout=PIPE).communicate()
+
+    return sorted(stdout.split(), key=lambda s: [x for x in s.replace('v', '').split('.')])
+
+
+def checkLastChangelogTag():
+    last = None
+    with open('CHANGELOG.md', 'r+') as f:
+        lines = f.readlines()
+
+        for line in lines:
+            m = re.search(r'^##\s+(\S+):', line)
+            if m:
+                last = m.group(1)
+                break
+
+    return last
+
+
+def buildNewLogs(fromTag, toTag):
+    stdout = ''
+    if fromTag:
+        (stdout, _) = Popen(('git rev-list %s..%s' % (fromTag, toTag)).split(), stdout=PIPE).communicate()
+    else:
+        (stdout, _) = Popen(('git rev-list %s' % toTag).split(), stdout=PIPE).communicate()
+
+    commits = stdout.splitlines()
+    feats = []
+    fixs = []
+    brokens = []
+
+    for commit in commits:
+        (title, _) = Popen(('git show -s --format=%%s %s' % commit).split(), stdout=PIPE).communicate()
+        (body, _) = Popen(('git show -s --format=%%b %s' % commit).split(), stdout=PIPE).communicate()
+        if not title:
+            continue
+
+        data = title.split(' ', 1)
+
+        if data[0] == 'feat':
+            feats.append(data[1].rstrip())
+        elif data[0] == 'fix':
+            fixs.append(data[1].rstrip())
+
+        if 'BROKEN:' in body:
+            brokens += body.split('BROKEN:')[1].splitlines()
+
+    logs = "## %s:\n" % toTag
+
+    if not len(feats) and not len(fixs) and not len(brokens):
+        logs += "*No major changes.*\n\n\n"
+    else:
+        if len(feats):
+            logs += "\n#### New features:\n"
+            for feat in feats:
+                logs += " - %s\n" % feat
+        if len(fixs):
+            logs += "\n#### Bug fixes:\n"
+            for fix in fixs:
+                logs += " - %s\n" % fix
+        if len(brokens):
+            logs += "\n#### Breaking changes:\n"
+            for broken in brokens:
+                if broken.rstrip() != '':
+                    logs += " - %s\n" % broken
+        logs += "\n\n"
+
+    return logs
+
+
+if __name__ == "__main__":
+    tags = getTags()
+    lastChangelogTag = checkLastChangelogTag()
+
+    changelog = ''
+    tagsToBuild = tags
+    previousTag = None
+    if lastChangelogTag:
+        previousTag = lastChangelogTag
+        tagsToBuild = tags[tags.index(lastChangelogTag) + 1:]
+    else:
+        tagsToBuild = tags[1:]  # ignoring first release which contains only the first commit
+
+    with open('CHANGELOG.md', 'r+') as f:
+        changelog = f.read().replace('# Changelog\n\n', '').rstrip() + '\n'
+
+    if not len(tagsToBuild):
+        print "No new changelogs! Last tag (%s) is already in the CHANGELOG.md." % lastChangelogTag
+        exit(0)
+
+    for tag in tagsToBuild:
+        newLogs = buildNewLogs(previousTag, tag)
+        previousTag = tag
+        changelog = newLogs + changelog
+
+    changelog = '# Changelog\n\n' + changelog
+
+    with open('CHANGELOG.md', 'w') as f:
+        f.write(changelog)
---
+++ release.py
@@ -0,0 +1,79 @@
+#! /usr/bin/env python
+
+import re
+import subprocess
+import sys
+
+
+def checkExistingTag(version):
+    if (subprocess.call(('git show-ref --verify --quiet refs/heads/%s' % version).split()) == 0 or
+            subprocess.call(('git show-ref --verify --quiet refs/tags/%s' % version).split()) == 0):
+        print "Error: The tag '%s' already exists" % version
+        raise Exception()
+
+
+# def checkout(node):
+#     if subprocess.call(('git checkout %s' % node).split(), stderr=subprocess.STDOUT, stdout=subprocess.PIPE) != 0:
+#         print "Error: The git node '%s' doesn't exist" % node
+#         exit(-1)
+
+
+def updateHomepage(version):
+    file_str = None
+    with open('build/includes/home/home.html') as f:
+        file_str = f.read()
+
+    file_str = re.sub(r'href="[^"]*"',
+                      'href="https://github.com/lumapps/lumX/archive/%s.zip"' % version,
+                      file_str)
+
+    file_str = re.sub(r'<span class="home-banner__version">[^"]*<\/span>',
+                      '<span class="home-banner__version">%s</span>' % version,
+                      file_str)
+
+    with open('demo/includes/home/home.html', "w") as f:
+        f.write(file_str)
+
+
+def updateGitignore():
+    file_str = None
+    with open('.gitignore') as f:
+        file_str = f.read()
+
+    file_str = re.sub(r'/dist', '', file_str)
+
+    with open('.gitignore', "w") as f:
+        f.write(file_str)
+
+
+def commit(version):
+    untrackedFiles = subprocess.Popen('git ls-files -o --exclude-standard'.split(), stdout=subprocess.PIPE)
+    subprocess.call(('git add %s' % untrackedFiles.stdout.read().replace('\n', ' ')).split())
+    subprocess.call(['git', 'commit', '-am', 'chore release: new release %s' % version], stderr=subprocess.STDOUT,
+                    stdout=subprocess.PIPE)
+    subprocess.call(('git tag %s' % version).split())
+    # print "Publishing new commit to master"
+    # subprocess.call('git push origin master'.split(), stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
+    print "Publishing new tag"
+    subprocess.call(('git push origin %s' % version).split(), stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
+    print "Release %s created!" % version
+
+
+if __name__ == "__main__":
+    try:
+        if len(sys.argv) == 1:
+            print "Error: The version name is required"
+            raise Exception()
+
+        version = sys.argv[1]
+
+        checkExistingTag(version)
+
+        # if len(sys.argv) > 2:
+        #     checkout(sys.argv[2])
+
+        updateHomepage(version)
+        updateGitignore()
+        commit(version)
+    except Exception as e:
+        exit(-1)
