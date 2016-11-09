cadfed7445d7cca44fb25cfe511327b2cc4faf5b
Clément PREVOT <clement.prevot@gmail.com>
2016-11-09 16:23:44 +0100

    fix(not-found): add missing selector for not-found component

--- src/client/app/core/settings/selectors.settings.ts
+++ src/client/app/core/settings/selectors.settings.ts
@@ -1,4 +1,6 @@
 export const SELECTOR_PREFIX: string = 'lx';
 export const SELECTOR_SEPARATOR: string = '-';
 
+export const NOT_FOUND_SELECTOR: string = 'not-found';
+
 export const DEMO_SELECTOR: string = 'demo';
