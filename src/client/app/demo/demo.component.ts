1553115ae195773ee5c6c3b94c00b86cd4b59cf3
Arnaud Valle <arnaud.valle@gmail.com>
2016-11-14 09:43:09 +0100

    feat(routing): Navigating to home or not toggles class on body

--- src/client/app/demo/demo.component.ts
+++ src/client/app/demo/demo.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit } from '@angular/core';
+import { ActivatedRoute, Event, NavigationEnd, Router } from '@angular/router';
 
-import { MobileNavService } from 'demo/services/mobile-nav.service';
+import 'rxjs/add/operator/filter';
 
 import { DEMO_SELECTOR as SELECTOR } from 'core/settings/selectors.settings';
 import { SELECTOR_PREFIX, SELECTOR_SEPARATOR } from 'core/settings/selectors.settings';
@@ -29,6 +30,35 @@ const template: string = require(`./${SELECTOR}.component.html`);
  * Demo Component.
  * Top Level Component.
  */
-export class DemoComponent {
-    constructor(private mobileNavService: MobileNavService) {}
+export class DemoComponent implements AfterViewInit, OnInit {
+    /**
+     * A reference to the <body> element
+     *
+     * @type {any}
+     * @private
+     */
+    private bodyEl: any;
+
+    /**
+     * Creates an instance of DemoComponent.
+     *
+     * @param {ElementRef} el A reference to the current component element
+     * @param {Router} r      The application router
+     *
+     */
+    constructor(private el: ElementRef,
+                private r: Router) {}
+
+    ngOnInit(): void {
+        // Listen to page navigation and update the <body> element accordingly
+        this.r.events
+            .filter((evt: Event) => evt instanceof NavigationEnd)
+            .subscribe((evt: NavigationEnd) => {
+                this.bodyEl.classList.toggle('home', evt.urlAfterRedirects === '/');
+            });
+    }
+
+    ngAfterViewInit(): void {
+        this.bodyEl = this.el.nativeElement.parentNode;
+    }
 }
