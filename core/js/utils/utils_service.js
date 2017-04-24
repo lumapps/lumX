ea311a0dfeea69c12ed4db8e5164b86c0ab0493d
Arnaud Valle <arnaud.valle@gmail.com>
2017-04-24 10:42:04 +0200

    feat: add a debounce function

--- core/js/utils/utils_service.js
+++ core/js/utils/utils_service.js
@@ -10,9 +10,58 @@
     {
         var service = this;
 
+        service.debounce = debounce;
         service.generateUUID = generateUUID;
 
         ////////////
+
+        // http://underscorejs.org/#debounce
+        function debounce(func, wait, immediate)
+        {
+            var timeout, args, context, timestamp, result;
+
+            wait = wait || 500;
+
+            var later = function()
+            {
+                var last = Date.now - timestamp;
+
+                if (last < wait && last >= 0)
+                {
+                    timeout = setTimeout(later, wait - last);
+                }
+                else
+                {
+                    timeout = null;
+                    if (!immediate)
+                    {
+                        result = func.apply(context, args);
+                        if (!timeout)
+                        {
+                            context = args = null;
+                        }
+                    }
+                }
+            };
+
+            return function() {
+                context = this;
+                args = arguments;
+                timestamp = Date.now;
+                var callNow = immediate && !timeout;
+                if (!timeout)
+                {
+                    timeout = setTimeout(later, wait);
+                }
+                if (callNow)
+                {
+                    result = func.apply(context, args);
+                    context = args = null;
+                }
+
+                return result;
+            };
+        }
 
         function generateUUID()
         {
