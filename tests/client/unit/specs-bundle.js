7143d765c8181206962cf61c9898c4aee8da1e8b
Clément PREVOT <clement@lumapps.com>
2017-04-27 12:07:30 +0200

    test(bundle): fix, update, improve and lint the test bundle file

--- tests/client/unit/specs-bundle.js
+++ tests/client/unit/specs-bundle.js
@@ -1,64 +1,61 @@
 /*
- * When testing with webpack and ES6, we have to do some extra
- * things to get testing to work right. Because we are gonna write tests
- * in ES6 too, we have to compile those as well. That's handled in
- * karma.conf.js with the karma-webpack plugin. This is the entry
- * file for webpack test. Just like webpack will create a bundle.js
- * file for our client, when we run test, it will compile and bundle them
- * all here! Crazy huh. So we need to do some setup
+ * When testing with webpack and ES6, we have to do some extra things to get testing to work right. Because we are gonna
+ * write tests in ES6 too, we have to compile those as well. That's handled in 'karma.conf.js' with the
+ * 'karma-webpack plugin'.
+ * This is the entry file for webpack test. Just like webpack will create a 'bundle.js' file for our client, when we run
+ * tests, it will compile and bundle them all here!
+ * Crazy huh.
+ * So we need to do some setup...
  */
 
 Error.stackTraceLimit = Infinity;
 
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 2000;
-
 /*
- * Import all SystemJS, CoreJS, TypeScript, Zone and RXJS polyfils
- * Order is important
+ * Import all SystemJS, CoreJS, TypeScript, Zone and RXJS polyfils.
+ * Order is important!
  */
 require('core-js/es6');
 require('core-js/es7/reflect');
-
-require('ts-helpers');
 
 require('zone.js/dist/zone');
 require('zone.js/dist/long-stack-trace-zone');
 require('zone.js/dist/proxy');
 require('zone.js/dist/sync-test');
-require('zone.js/dist/jasmine-patch');
+require('zone.js/dist/mocha-patch');
 require('zone.js/dist/async-test');
 require('zone.js/dist/fake-async-test');
 
 require('rxjs/Rx');
 
 /*
- * Initialize test environment
+ * Initialize test environment.
  */
-var TestBed = require('@angular/core/testing').TestBed;
-var BrowserDynamicTestingModule = require('@angular/platform-browser-dynamic/testing').BrowserDynamicTestingModule;
-var platformBrowserDynamicTesting = require('@angular/platform-browser-dynamic/testing').platformBrowserDynamicTesting;
+const TestBed = require('@angular/core/testing').TestBed;
+const BrowserDynamicTestingModule = require('@angular/platform-browser-dynamic/testing').BrowserDynamicTestingModule;
+const platformBrowserDynamicTesting =
+    require('@angular/platform-browser-dynamic/testing').platformBrowserDynamicTesting;
 
 TestBed.initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
 
 /*
- * Ok, this is kinda crazy. We can use the context method on
- * require that webpack created in order to tell webpack
- * what files we actually want to require or import.
+ * Ok, this is kinda crazy.
+ * We can use the context method on require that webpack created in order to tell webpack what files we actually want to
+ * require or import.
  * Below, context will be a function/object with file names as keys.
- * Using that regex we are saying look in ../src then find
- * any file that ends with spec.ts and get its path. By passing in true
- * we say do this recursively
+ * Using that regex we are saying look in '../../../src/client/app' then find any file that ends with 'spec(s).ts' and
+ * get its path.
+ * By passing in true we say do this recursively.
  */
-var testContext = require.context('../../../src/client/app', true, /\.spec|specs\.ts/);
+const testContext = require.context('../../../src/client/app', true, /\.spec|specs\.ts/);
 
 /*
- * Get all the files, for each file, call the context function
- * that will require the file and load it up here.
- * Context will loop and require those spec files here
+ * Get all the files.
+ * For each file, call the context function that will require the file and load it up here.
+ * Context will loop and require those spec files here.
  */
 function requireAll(requireContext) {
     return requireContext.keys().map(requireContext);
 }
 
-// Requires and returns all modules that match
+// Requires and returns all modules that match.
 requireAll(testContext);
