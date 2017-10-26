f128cef75a737a99098c9bbccb5a5825695cbd03
Clément PREVOT <clement@lumapps.com>
2017-10-26 12:45:26 +0200

    feat utils: add a function to escape regexp char in a string
    This will be useful for future features.
    Also, reorder the function in the utils service

--- core/js/utils/utils_service.js
+++ core/js/utils/utils_service.js
@@ -11,8 +11,9 @@
         var service = this;
 
         service.debounce = debounce;
-        service.generateUUID = generateUUID;
         service.disableBodyScroll = disableBodyScroll;
+        service.escapeRegexp = escapeRegexp;
+        service.generateUUID = generateUUID;
 
         ////////////
 
@@ -73,21 +74,6 @@
             return debounced;
         }
 
-        function generateUUID()
-        {
-            var d = new Date().getTime();
-
-            var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c)
-            {
-                var r = (d + Math.random() * 16) % 16 | 0;
-                d = Math.floor(d / 16);
-                return (c == 'x' ? r : (r & 0x3 | 0x8))
-                    .toString(16);
-            });
-
-            return uuid.toUpperCase();
-        }
-
         function disableBodyScroll()
         {
             var body = document.body;
@@ -138,6 +124,31 @@
                 body.scrollTop = viewportTop;
               }
             };
+        }
+
+        /**
+         * Escape all RegExp special characters in a string.
+         *
+         * @param  {string} strToEscape The string to escape RegExp special char in.
+         * @return {string} The escapes string.
+         */
+        function escapeRegexp(strToEscape) {
+            return strToEscape.replace(/([.?*+^$[\]\\(){}|-])/g, '\\$1');
+        }
+
+        function generateUUID()
+        {
+            var d = new Date().getTime();
+
+            var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c)
+            {
+                var r = (d + Math.random() * 16) % 16 | 0;
+                d = Math.floor(d / 16);
+                return (c == 'x' ? r : (r & 0x3 | 0x8))
+                    .toString(16);
+            });
+
+            return uuid.toUpperCase();
         }
     }
 })();
