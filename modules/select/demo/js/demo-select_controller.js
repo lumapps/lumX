257a1aa6c3849fd173899fd7ff299954c63823b2
Florian Nourrisse <florian.nourrisse@lumapps.com>
2019-03-14 17:12:32 +0100

    feat(infinite-scroll): add infinite scroll attribute to lx-select + add documentation

--- modules/select/demo/js/demo-select_controller.js
+++ modules/select/demo/js/demo-select_controller.js
@@ -1,154 +1,211 @@
-(function()
-{
+(function() {
     'use strict';
 
     angular
         .module('Controllers')
         .controller('DemoSelectController', DemoSelectController);
 
-    DemoSelectController.$inject = ['$http', '$scope', '$timeout', 'LxNotificationService'];
+    DemoSelectController.$inject = [
+        '$http',
+        '$scope',
+        '$timeout',
+        'LxNotificationService'
+    ];
 
-    function DemoSelectController($http, $scope, $timeout, LxNotificationService)
-    {
+    function DemoSelectController(
+        $http,
+        $scope,
+        $timeout,
+        LxNotificationService
+    ) {
         var vm = this;
 
+        vm.callApi = callApi;
         vm.selectCallback = selectCallback;
 
         vm.selectAjax = {
             selected: 'Darth Vader',
             list: [],
-            update: function(newFilter)
-            {
-                if (newFilter)
-                {
+            update: function(newFilter) {
+                if (newFilter) {
                     vm.selectAjax.loading = true;
 
-                    $http.get('https://swapi.co/api/people/?search=' + escape(newFilter))
-                        .then(function updateSuccess(response)
-                        {
-                            if (response.data && response.data.results)
-                            {
+                    $http
+                        .get(
+                            'https://swapi.co/api/people/?search=' +
+                                escape(newFilter)
+                        )
+                        .then(function updateSuccess(response) {
+                            if (response.data && response.data.results) {
                                 vm.selectAjax.list = response.data.results;
                             }
                             vm.selectAjax.loading = false;
                         })
-                        .catch(function updateError()
-                        {
+                        .catch(function updateError() {
                             vm.selectAjax.loading = false;
                         });
-                }
-                else
-                {
+                } else {
                     vm.selectAjax.list = false;
                 }
             },
-            toModel: function(data, callback)
-            {
-                if (data)
-                {
+            toModel: function(data, callback) {
+                if (data) {
                     callback(data.name);
-                }
-                else
-                {
+                } else {
                     callback();
                 }
             },
-            toSelection: function(data, callback)
-            {
-                if (data)
-                {
-                    $http.get('https://swapi.co/api/people/?search=' + escape(data))
-                        .then(function toSelectionSuccess(response)
-                        {
-                            if (response.data && response.data.results)
-                            {
+            toSelection: function(data, callback) {
+                if (data) {
+                    $http
+                        .get(
+                            'https://swapi.co/api/people/?search=' +
+                                escape(data)
+                        )
+                        .then(function toSelectionSuccess(response) {
+                            if (response.data && response.data.results) {
                                 callback(response.data.results[0]);
                             }
                         })
-                        .catch(function toSelectionError()
-                        {
+                        .catch(function toSelectionError() {
                             callback();
                         });
-                }
-                else
-                {
+                } else {
                     callback();
                 }
             },
             loading: false
         };
         vm.selectPeople = [
-        {
-            name: 'Adam',
-            email: 'adam@email.com',
-            age: 10
-        },
-        {
-            name: 'Amalie',
-            email: 'amalie@email.com',
-            age: 12
-        },
-        {
-            name: 'Wladimir',
-            email: 'wladimir@email.com',
-            age: 30
-        },
-        {
-            name: 'Samantha',
-            email: 'samantha@email.com',
-            age: 31
-        },
-        {
-            name: 'Estefanía',
-            email: 'estefanía@email.com',
-            age: 16
-        },
-        {
-            name: 'Natasha',
-            email: 'natasha@email.com',
-            age: 54
-        },
-        {
-            name: 'Nicole',
-            email: 'nicole@email.com',
-            age: 43
-        },
-        {
-            name: 'Adrian',
-            email: 'adrian@email.com',
-            age: 21
-        }];
+            {
+                name: 'Adam',
+                email: 'adam@email.com',
+                age: 10
+            },
+            {
+                name: 'Amalie',
+                email: 'amalie@email.com',
+                age: 12
+            },
+            {
+                name: 'Wladimir',
+                email: 'wladimir@email.com',
+                age: 30
+            },
+            {
+                name: 'Samantha',
+                email: 'samantha@email.com',
+                age: 31
+            },
+            {
+                name: 'Estefanía',
+                email: 'estefanía@email.com',
+                age: 16
+            },
+            {
+                name: 'Natasha',
+                email: 'natasha@email.com',
+                age: 54
+            },
+            {
+                name: 'Nicole',
+                email: 'nicole@email.com',
+                age: 43
+            },
+            {
+                name: 'Adrian',
+                email: 'adrian@email.com',
+                age: 21
+            },
+            {
+                name: 'David',
+                email: 'david@email.com',
+                age: 42
+            },
+            {
+                name: 'Julia',
+                email: 'julia@email.com',
+                age: 35
+            },
+            {
+                name: 'Ahmed',
+                email: 'ahmed@email.com',
+                age: 31
+            },
+            {
+                name: 'Sofia',
+                email: 'sofia@email.com',
+                age: 23
+            },
+            {
+                name: 'Corinne',
+                email: 'corinne@email.com',
+                age: 54
+            },
+            {
+                name: 'Nicolas',
+                email: 'nicolas@email.com',
+                age: 32
+            },
+            {
+                name: 'Henry',
+                email: 'henry@email.com',
+                age: 18
+            },
+            {
+                name: 'Carol',
+                email: 'carol@email.com',
+                age: 45
+            },
+            {
+                name: 'Stephan',
+                email: 'stephan@email.com',
+                age: 36
+            },
+            {
+                name: 'Elizabeth',
+                email: 'elizabeth@email.com',
+                age: 92
+            },
+            {
+                name: 'François',
+                email: 'francois@email.com',
+                age: 45
+            }
+        ];
         vm.selectSections = {
             '<i class="mdi mdi-account"></i> <span>Sub header 1</span>': [
-            {
-                uid: '1',
-                name: 'Adam'
-            },
-            {
-                uid: '2',
-                name: 'Amalie'
-            },
-            {
-                uid: '3',
-                name: 'Wladimir'
-            },
-            {
-                uid: '4',
-                name: 'Samantha'
-            }],
+                {
+                    uid: '1',
+                    name: 'Adam'
+                },
+                {
+                    uid: '2',
+                    name: 'Amalie'
+                },
+                {
+                    uid: '3',
+                    name: 'Wladimir'
+                },
+                {
+                    uid: '4',
+                    name: 'Samantha'
+                }
+            ],
             '<i class="mdi mdi-account"></i> <span>Sub header 2</span>': [
-            {
-                uid: '5',
-                name: 'Estefanía'
-            },
-            {
-                uid: '6',
-                name: 'Natasha'
-            },
-            {
-                uid: '7',
-                name: 'Nicole'
-            }]
+                {
+                    uid: '5',
+                    name: 'Estefanía'
+                },
+                {
+                    uid: '6',
+                    name: 'Natasha'
+                },
+                {
+                    uid: '7',
+                    name: 'Nicole'
+                }
+            ]
         };
         vm.selectVegetables = [
             {
@@ -166,7 +223,7 @@
         ];
         vm.selectPeopleMultipane = {
             '<i class="mdi mdi-human-male"></i> <span>Male humans</span>': {
-                'Old': [
+                Old: [
                     {
                         uid: '1',
                         name: 'Adam'
@@ -186,7 +243,7 @@
                         name: 'Franck'
                     }
                 ],
-                'Young': [
+                Young: [
                     {
                         uid: '5',
                         name: 'Wladimir'
@@ -195,18 +252,18 @@
                         uid: '6',
                         name: 'Jack'
                     }
-                ],
+                ]
             },
             '<i class="mdi mdi-human-female"></i> <span>Female</span>': {
-                'France': {
-                    'Old': [
+                France: {
+                    Old: [
                         {
                             uid: '7',
                             name: 'Sue Helen'
                         }
                     ],
                     'Middle aged': {
-                        'Lorem': [
+                        Lorem: [
                             {
                                 uid: '8',
                                 name: 'Samantha'
@@ -272,7 +329,7 @@
                                 name: 'Karen'
                             }
                         ],
-                        'Ipsum': [
+                        Ipsum: [
                             {
                                 uid: '16',
                                 name: 'Julia'
@@ -281,9 +338,9 @@
                                 uid: '17',
                                 name: 'Sofia'
                             }
-                        ],
+                        ]
                     },
-                    'Young': [
+                    Young: [
                         {
                             uid: '18',
                             name: 'Karin'
@@ -292,9 +349,9 @@
                             uid: '19',
                             name: 'Camillia'
                         }
-                    ],
+                    ]
                 },
-                'US': [
+                US: [
                     {
                         uid: '16',
                         name: 'Katerin'
@@ -303,7 +360,7 @@
                         uid: '17',
                         name: 'Olga'
                     }
-                ],
+                ]
             }
         };
         vm.newValueTransformer = function(_newValue) {
@@ -322,6 +379,7 @@
             selectedVegetables: []
         };
         vm.multipaneLoading = true;
+        vm.pageInfiniteScroll = 0;
 
         $scope.$on('lx-dropdown__open-end', function onDropdownOpenEnd() {
             $timeout(function delayEndLoading() {
@@ -331,12 +389,40 @@
 
         ////////////
 
-        function selectCallback(_newValue, _oldValue)
-        {
+        function selectCallback(_newValue, _oldValue) {
             LxNotificationService.notify('Change detected');
 
             console.log('Old value: ', _oldValue);
             console.log('New value: ', _newValue);
+        }
+
+        /**
+         * Call sample API.
+         *
+         * @return {Promise} Promise containing an array of users.
+         */
+        function callApi() {
+            vm.pageInfiniteScroll++;
+            return $http
+                .get(
+                    `https://randomuser.me/api/?results=10&seed=lumapps&page=${
+                        vm.pageInfiniteScroll
+                    }`
+                )
+                .then(response => {
+                    if (response.data && response.data.results) {
+                        return response.data.results.map(person => ({
+                            name: person.name.first,
+                            email: person.email,
+                            age: person.dob.age
+                        }));
+                    } else {
+                        return [];
+                    }
+                })
+                .catch(() => {
+                    return [];
+                });
         }
     }
 })();
