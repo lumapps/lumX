7e140f3c1c9f0ee654b417e90c45dcb57afb502d
Matthias Manoukian <matthias.manoukian@gmail.com>
2019-10-08 16:52:18 +0200

    fix(tooltip): don't use popperjs anymore

--- modules/tooltip/js/tooltip_directive.js
+++ modules/tooltip/js/tooltip_directive.js
@@ -1,10 +1,8 @@
-import PopperJs from 'popper.js';
-
 import { CSS_PREFIX } from '@lumx/core/js/constants';
 
 /////////////////////////////
 
-function TooltipController($element, $timeout, LxDepthService) {
+function TooltipController($element, $timeout, $window) {
     'ngInject';
 
     // eslint-disable-next-line consistent-this
@@ -24,6 +22,15 @@ function TooltipController($element, $timeout, LxDepthService) {
      * @readonly
      */
     const _HOVER_DELAY = 500;
+
+    /**
+     * The offset from the target.
+     *
+     * @type {number}
+     * @constant
+     * @readonly
+     */
+    const _OFFSET = 8;
 
     /**
      * The tooltip element.
@@ -66,19 +73,39 @@ function TooltipController($element, $timeout, LxDepthService) {
      * Set the tooltip position according to the position parameter.
      */
     function _setTooltipPosition() {
-        _tooltip.appendTo('body');
+        const targetProps = {
+            height: $element.outerHeight(),
+            left: $element.offset().left,
+            top: $element.offset().top - angular.element($window).scrollTop(),
+            width: $element.outerWidth(),
+        };
+        const tooltipPosition = angular.isDefined(lx.position) ? lx.position : 'top';
+        const tooltipProps = {};
 
-        // eslint-disable-next-line no-new
-        new PopperJs($element, _tooltip, {
-            placement: lx.position || 'top',
-            modifiers: {
-                arrow: {
-                    // eslint-disable-next-line id-blacklist
-                    element: `.${CSS_PREFIX}-tooltip__arrow`,
-                    enabled: true,
-                },
-            },
+        _tooltip
+            .addClass(`${CSS_PREFIX}-tooltip--position-${tooltipPosition}`)
+            .css({ position: 'absolute' })
+            .appendTo('body');
+
+        /* eslint-disable no-magic-numbers */
+        if (tooltipPosition === 'top') {
+            tooltipProps.x = targetProps.left - _tooltip.outerWidth() / 2 + targetProps.width / 2;
+            tooltipProps.y = targetProps.top - _tooltip.outerHeight() - _OFFSET;
+        } else if (tooltipPosition === 'bottom') {
+            tooltipProps.x = targetProps.left - _tooltip.outerWidth() / 2 + targetProps.width / 2;
+            tooltipProps.y = targetProps.top + targetProps.height + _OFFSET;
+        } else if (tooltipPosition === 'left') {
+            tooltipProps.x = targetProps.left - _tooltip.outerWidth() - _OFFSET;
+            tooltipProps.y = targetProps.top + targetProps.height / 2 - _tooltip.outerHeight() / 2;
+        } else if (tooltipPosition === 'right') {
+            tooltipProps.x = targetProps.left + targetProps.width + _OFFSET;
+            tooltipProps.y = targetProps.top + targetProps.height / 2 - _tooltip.outerHeight() / 2;
+        }
+
+        _tooltip.css({
+            transform: `translate3d(${tooltipProps.x}px, ${tooltipProps.y}px, 0px)`,
         });
+        /* eslint-enable no-magic-numbers */
     }
 
     /////////////////////////////
@@ -105,7 +132,7 @@ function TooltipController($element, $timeout, LxDepthService) {
      * Show the tooltip on source element mouse enter.
      */
     function showTooltip() {
-        if (angular.isDefined(_tooltip) || !lx.text) {
+        if (angular.isDefined(_tooltip)) {
             return;
         }
 
@@ -122,12 +149,7 @@ function TooltipController($element, $timeout, LxDepthService) {
             text: lx.text,
         });
 
-        LxDepthService.increase();
-
-        _tooltip
-            .append(_tooltipArrow)
-            .append(_tooltipInner)
-            .css('z-index', LxDepthService.get());
+        _tooltip.append(_tooltipArrow).append(_tooltipInner);
 
         _hoverTimeout = $timeout(_setTooltipPosition, _HOVER_DELAY);
     }
