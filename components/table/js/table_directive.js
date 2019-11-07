3a374b075d44d2884426208182bea9fe672c5277
Matthias Manoukian <matthias.manoukian@gmail.com>
2019-11-07 14:34:14 +0100

    chore(table): use ng-class

--- components/table/js/table_directive.js
+++ components/table/js/table_directive.js
@@ -1,3 +1,5 @@
+import { CSS_PREFIX } from '@lumx/core/js/constants';
+
 import template from '../views/table.html';
 
 /////////////////////////////
@@ -5,8 +7,62 @@ import template from '../views/table.html';
 function TableController() {
     'ngInject';
 
-    // eslint-disable-next-line consistent-this, no-unused-vars
+    // eslint-disable-next-line consistent-this
     const lx = this;
+
+    /////////////////////////////
+    //                         //
+    //    Private attributes   //
+    //                         //
+    /////////////////////////////
+
+    /**
+     * The default props.
+     *
+     * @type {Object}
+     * @constant
+     * @readonly
+     */
+    const _DEFAULT_PROPS = {
+        theme: 'light',
+    };
+
+    /////////////////////////////
+    //                         //
+    //     Public functions    //
+    //                         //
+    /////////////////////////////
+
+    /**
+     * Get table classes.
+     *
+     * @return {Array} The list of table classes.
+     */
+    function getClasses() {
+        const classes = [];
+
+        const theme = lx.theme ? lx.theme : _DEFAULT_PROPS.theme;
+
+        classes.push(`${CSS_PREFIX}-table--theme-${theme}`);
+
+        if (lx.hasBefore) {
+            classes.push(`${CSS_PREFIX}-table--has-before`);
+        }
+
+        if (lx.hasDividers) {
+            classes.push(`${CSS_PREFIX}-table--has-dividers`);
+        }
+
+        if (lx.isClickable) {
+            classes.push(`${CSS_PREFIX}-table--is-clickable`);
+        }
+
+        return classes;
+    }
+
+    /////////////////////////////
+
+    lx.getClasses = getClasses;
 }
 
 /////////////////////////////
