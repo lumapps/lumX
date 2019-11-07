bbdf4c6e3ba2562abd5dd1d3cf51c2ba8b1a6edf
Matthias Manoukian <matthias.manoukian@gmail.com>
2019-11-07 13:54:17 +0100

    chore(progress): use ng-class

--- components/progress/js/progress_directive.js
+++ components/progress/js/progress_directive.js
@@ -1,3 +1,5 @@
+import { CSS_PREFIX } from '@lumx/core/js/constants';
+
 import template from '../views/progress.html';
 
 /////////////////////////////
@@ -5,8 +7,57 @@ import template from '../views/progress.html';
 function ProgressController() {
     'ngInject';
 
-    // eslint-disable-next-line consistent-this, no-unused-vars
+    // eslint-disable-next-line consistent-this
     const lx = this;
+
+    /////////////////////////////
+    //                         //
+    //    Private attributes   //
+    //                         //
+    /////////////////////////////
+
+    /**
+     * The default props.
+     *
+     * @type {Object}
+     * @constant
+     * @readonly
+     */
+    const _DEFAULT_PROPS = {
+        theme: 'light',
+        variant: 'circular',
+    };
+
+    /////////////////////////////
+    //                         //
+    //     Public functions    //
+    //                         //
+    /////////////////////////////
+
+    /**
+     * Get checkbox classes.
+     *
+     * @return {Array} The list of checkbox classes.
+     */
+    function getClasses() {
+        const classes = [];
+
+        const theme = lx.theme ? lx.theme : _DEFAULT_PROPS.theme;
+        const variant = lx.variant ? lx.variant : _DEFAULT_PROPS.variant;
+
+        classes.push(`${CSS_PREFIX}-progress--theme-${theme}`);
+        classes.push(`${CSS_PREFIX}-progress--variant-${variant}`);
+
+        if (lx.customColors) {
+            classes.push(`${CSS_PREFIX}-custom-colors`);
+        }
+
+        return classes;
+    }
+
+    /////////////////////////////
+
+    lx.getClasses = getClasses;
 }
 
 /////////////////////////////
